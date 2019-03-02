0017dfed81edc285d2a71c2dfbb260addc9c7dae
Tim <groeten.aan.tim@hotmail.com>
2019-03-02 15:16:12 +0100

    jannes magic file

--- experiment.py
+++ experiment.py
@@ -1,9 +1,12 @@
 import os
 import sys
 from tqdm import tqdm
+import time
 
 DIR = sys.argv[1]
 for root, subdirs, files in os.walk(DIR):
     for f in tqdm(files):
         p = os.path.join(root, f)
-        os.system('python3 SAT -S2 -Oresults_grab_first_1000.csv ' + p)
+        for i in range(1, 5):
+            os.system('python3 SAT -S{} -Oresults/{}__1000.csv {} &'.format(i, i, p))
+        time.sleep(2)
